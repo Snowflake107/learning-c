6bc9d89d642b5dc2a148743dad080a89dff24ebb
Snowflake <devsnowflake107@gmail.com>
2020-12-23 10:24:15 +0545

    Create beeptest.c

---
+++ basics/beeptest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main() {
+    
+    printf("test 1");
+    Beep(100, 1000);
+
+    Sleep(3000);
+
+    printf("test 2");
+    Beep(200, 1000);
+
+    return 0;
+}
