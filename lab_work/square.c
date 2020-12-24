6f2558f3f79c388a811720f791eed11a6c561d3f
Snowflake <devsnowflake107@gmail.com>
2020-12-24 15:56:11 +0545

    Create square.c

---
+++ lab_work/square.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main() {
+    double num, sqr;
+
+    printf("\nEnter a number: ");
+    scanf("%lf", &num);
+
+    sqr = sqrt(num);
+
+    printf("\nSquare root of %.0lf is: %.0lf\n", num, sqr);
+
+    getch();
+
+    return 0;
+}
