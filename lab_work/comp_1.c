5e79c677e9e8be3faffcc72a67d63b4a3a7acc36
Snowflake <dhunganakunjan1@gmail.com>
2020-12-24 17:21:56 +0545

    comp

---
+++ lab_work/comp_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+    float x;
+    int x1 = 7;
+    int x2 = 2;
+
+    x = (float) x1 / x2;
+    printf("%f", x);
+
+    getch();
+    return 0;
+}
+
+// output: 3.500000
---
+++ lab_work/comp_3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+    int j;
+    int i = 5;
+
+    j = ++i;
+
+    printf("i=%d\nj=%d", i, j);
+
+    getch();
+    return 0;
+}
+
+// output:
+// i = 6
+// j = 6
