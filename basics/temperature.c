2e83b407c79b4ae707ba5f11ab1f02c2a0945d51
Snowflake <devsnowflake107@gmail.com>
2020-12-24 08:32:57 +0545

    Update temperature.c

--- basics/temperature.c
+++ basics/temperature.c
@@ -1,18 +1,35 @@
 #include<stdio.h>
 #include<conio.h>
 
+// Cel to fah
 float celtofah(float deg) {
     return (deg * 9 / 5) + 32;
 }
 
+// Cel to kel
+float celtokel(float deg) {
+    return deg + 273.15;
+}
+
+// fah to cel
 float fahtocel(float deg) {
     return (deg - 32) * 5 / 9;
 }
 
+// fah to kel
 float fahtokel(float deg) {
     return (deg - 32) * 5 / 9 + 273.15;
 }
 
+// kel to cel
+float keltocel(float deg) {
+    return deg - 273.15;
+}
+
+// kel to fah
+float keltofah(float deg) {
+    return (deg - 273.15) * 9 / 5 + 32;
+}
 
 int main() {
     float cen, fah, kel;
